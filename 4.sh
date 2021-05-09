f25b98117e1cd52664da56eeb50c20c937aee9f6
Esteban J Guillen <esteban@Estebans-MacBook-Pro-3.local>
2021-05-09 02:49:34 -0600

    add exeriment scripts

---
+++ 4.sh
@@ -0,0 +1,6 @@
+python train.py --arch cnn --lr 1e-4 --optim adam --pre_trained no --data_aug yes —gpu 0
+python train.py --arch cnn --lr 1e-4 --optim adam --pre_trained yes --data_aug yes —gpu 0
+python train.py --arch cnn --lr 1e-4 --optim sgd --pre_trained no --data_aug no —gpu 0
+python train.py --arch cnn --lr 1e-4 --optim sgd --pre_trained yes --data_aug no —gpu 0
+python train.py --arch cnn --lr 1e-4 --optim adam --pre_trained no --data_aug no —gpu 0
+python train.py --arch cnn --lr 1e-4 --optim adam --pre_trained yes --data_aug no —gpu 0
---
+++ 7.sh
@@ -0,0 +1,4 @@
+python train.py --arch vit --lr 1e-5 --optim sgd --pre_trained no --data_aug yes —gpu 2
+python train.py --arch vit --lr 1e-5 --optim sgd --pre_trained yes --data_aug yes —gpu 2
+python train.py --arch vit --lr 1e-5 --optim adam --pre_trained no --data_aug yes —gpu 2
+python train.py --arch vit --lr 1e-5 --optim adam --pre_trained yes --data_aug yes —gpu 2
